4ffc9ab7781b24e8cae828856aea7a30f8d19361
carlos-ERL <caedrobleslo@ittepic.edu.mx>
2022-05-01 01:20:43 -0600

    Primer Commit

---
+++ app/src/main/java/mx/edu/ittepic/ladm_u3_practica1_veterinaria/ui/gallery/GalleryViewModel.kt
@@ -0,0 +1,13 @@
+package mx.edu.ittepic.ladm_u3_practica1_veterinaria.ui.gallery
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GalleryViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is gallery Fragment"
+    }
+    val text: LiveData<String> = _text
+}
